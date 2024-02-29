a3056fd0bc55468a22e8c43a9439ff31258855e8
Lukas Ribi <lukas.ribi@griesser.ch>
2024-02-29 14:36:53 +0100

    removed depricated patterns

--- rbautofillbug/admin_urls.py
+++ rbautofillbug/admin_urls.py
@@ -1,15 +1,13 @@
-from __future__ import unicode_literals
-
-from django.conf.urls import patterns, url
+from django.urls import path
+from reviewboard.extensions.views import configure_extension
 
 from rbautofillbug.extension import AutoFillBugExtension
 from rbautofillbug.forms import AutoFillBugExtensionSettingsForm
 
-urlpatterns = patterns('',
-    url(r'^$',
-        'reviewboard.extensions.views.configure_extension',
-        {
-            'ext_class': AutoFillBugExtension,
-            'form_class': AutoFillBugExtensionSettingsForm,
-        }),
-)
+urlpatterns = [path('',
+                    configure_extension,
+                    {
+                        'ext_class': AutoFillBugExtension,
+                        'form_class': AutoFillBugExtensionSettingsForm,
+                    }),
+               ]
